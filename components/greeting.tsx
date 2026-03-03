aae0875ec1c3df5762dd833806f4ffd48dd6a3c5
subham_ttl <sjj930967@tatatechnologies.com>
2026-03-03 15:26:46 +0530

    ui changes to remove deafult behaviour

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -22,7 +22,7 @@ export const Greeting = () => {
         initial={{ opacity: 0, y: 10 }}
         transition={{ delay: 0.6 }}
       >
-        How can I help you today?
+        Coversational PLM is ready .. 
       </motion.div>
     </div>
   );
--- components/suggested-actions.tsx
+++ components/suggested-actions.tsx
@@ -15,10 +15,10 @@ type SuggestedActionsProps = {
 
 function PureSuggestedActions({ chatId, sendMessage }: SuggestedActionsProps) {
   const suggestedActions = [
-    "What are the advantages of using Next.js?",
-    "Write code to demonstrate Dijkstra's algorithm",
-    "Help me write an essay about Silicon Valley",
-    "What is the weather in San Francisco?",
+  //   "What are the advantages of using Next.js",
+  //   "Write code to demonstrate Dijkstra's algorithm",
+  //   "Help me write an essay about Silicon Valley",
+  //   "What is the weather in San Francisco?",
   ];
 
   return (
